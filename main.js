cf79c9f1f9ee5bb9254ed291da89aeadec5f17f0
Samuel Trevan <samuel.trevan@gmail.com>
2022-10-07 09:28:34 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// Take the 5 values assigned to the 5 variables and store them in the provided blank array instead. 
+// When you are done, there should be no variables with string values left in your code - only the fruits variable.
+
+const yellowFruit = "Banana"
+const orangeFruit = "Orange"
+const redFruit = "Apple"
+const greenFruit = "Watermelon"
+const blueFruit = "Blueberry"
+
+const fruits = [yellowFruit, orangeFruit, redFruit, greenFruit, blueFruit]
+
+console.log(fruits)
